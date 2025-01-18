db0f23bd76cb549c14e6296ef89baeab5c61d0f6
Aditya.Pandit <adpndt@gmail.com>
2025-01-18 19:16:20 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,9 @@ set -e
 # Configure the GitHub Actions Runner
 
 
+
 export RUNNER_ALLOW_RUNASROOT=true
+./config.sh remove --token ${RUNNER_TOKEN}
 ./config.sh --url ${REPO_URL} --token ${RUNNER_TOKEN} --name ${RUNNER_NAME} --work ${RUNNER_WORKDIR} --unattended --replace
 
 # Start the runner
