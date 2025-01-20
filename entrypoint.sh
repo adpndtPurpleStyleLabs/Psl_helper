14662e2e859b2d5345925b9ac111deb75f342f9a
Aditya.Pandit <adpndt@gmail.com>
2025-01-20 12:01:53 +0530

    added check if that runner is defined

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
 
-# Configure the GitHub Actions Runner
-
-
-
 export RUNNER_ALLOW_RUNASROOT=true
-./config.sh remove --token ${RUNNER_TOKEN}
-./config.sh --url ${REPO_URL} --token ${RUNNER_TOKEN} --name ${RUNNER_NAME} --work ${RUNNER_WORKDIR} --unattended --replace
+RUNNER_FILE="/tmp/runner-workdir/runner_name.txt"
+if [[ -f "$RUNNER_FILE" ]] && grep -q "$RUNNER_NAME" "$RUNNER_FILE"; then
+      echo "Runner name already exists in $RUNNER_FILE. Proceeding..."
+else
+      echo "Runner name not found in $RUNNER_FILE. Configuring the runner..."
+      ./config.sh remove --token ${RUNNER_TOKEN}
+      ./config.sh --url ${REPO_URL} --token ${RUNNER_TOKEN} --name ${RUNNER_NAME} --work ${RUNNER_WORKDIR} --unattended --replace
+      mkdir -p /tmp/runner-workdir
+      echo "$RUNNER_NAME" > "$RUNNER_FILE"
+fi
 
-# Start the runner
 ./run.sh
+
