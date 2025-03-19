d4f16a1760997c9995f948436244c7d615f5d385
Aditya.Pandit <adpndt@gmail.com>
2025-03-19 11:45:43 +0530

    seema gujram invoice no corrections

--- VendorsInvoicePdfToExcel/VendorImplementations/SeemaGujral.py
+++ VendorsInvoicePdfToExcel/VendorImplementations/SeemaGujral.py
@@ -23,7 +23,7 @@ class SeemaGujral:
     def getInvoiceInfo(self):
         firstPage = self.text[1].split("\n")
         return {
-            "invoice_number": get_list_containing(firstPage, "Invoice no").split("\n")[-1].strip(),
+            "invoice_number": get_list_containing(self.tables[1], "Invoice no").split("\n")[-1].strip(),
             "invoice_date": firstPage[indexOfContainsInList(firstPage, "Dated") + 1].split(" ")[-1]
         }
 
